895c648bcd936be67cd38e49761243f514a30325
Vinícius Aquino <vnaquino99@gmail.com>
2025-07-03 22:01:11 -0300

    agora foi

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toggleBtn = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
+const homeBtn = document.getElementById('home-btn');
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
@@ -16,3 +17,42 @@ document.addEventListener('click', (e) => {
     menu.classList.remove('active');
   }
 });
+
+window.addEventListener('scroll', () => {
+    const hero = document.getElementById('hero');
+    const scrollPosition = window.scrollY;
+
+    if (window.scrollY > 100) {
+        homeBtn.classList.remove('hidden');
+    } else {
+        homeBtn.classList.add('hidden');
+    }
+
+    hero.style.backgroundPositionY = `${scrollPosition * 0.5}px`
+}, {passive: true});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            entry.target.classList.remove('invisible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+document.querySelectorAll('.bloco').forEach(bloco => {
+    observer.observe(bloco);
+})
+
+document.querySelectorAll('.curso').forEach(curs => {
+    observer.observe(curs);
+} )
+
+document.querySelectorAll('.saidas').forEach(saida => {
+    observer.observe(saida);
+})
+
+observer.observe(document.querySelector('.icones'));
